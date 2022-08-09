606311935ad2bde63b7b95b5632af44ca1955d3f
fast-vid2vid <108454075+fast-vid2vid@users.noreply.github.com>
2022-08-09 17:04:48 +0800

    update

--- scripts/face/train_tkd.sh
+++ scripts/face/train_tkd.sh
@@ -1,4 +1,4 @@
-  python train_tkd.py --name label2city_512_student_tkd \
+  python train_tkd.py --name edge2face_student_tkd \
   --dataroot datasets/face/ --dataset_mode face --input_nc 15 --loadSize 512 --num_D 3 \
   --gpu_ids 0 --n_gpus_gen -1 \
   --n_frames_total 6 --batchSize 1 --nThread 1 \
