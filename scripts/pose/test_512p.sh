826c9f1bfdc68b1bd6a3312fd5b694fcb1df1efa
fast-vid2vid <108454075+fast-vid2vid@users.noreply.github.com>
2023-01-16 23:31:02 +0800

    Delete test_512p.sh

--- scripts/pose/test_512p.sh
+++
@@ -1,5 +0,0 @@
-python test.py --name pose2body_512p \
---dataroot datasets/Pose --dataset_mode pose \
---netG sr_mobile_composite \
---input_nc 6 --n_scales_spatial 1 \
---resize_or_crop scaleHeight --loadSize 512 --use_real_img --phase train 
